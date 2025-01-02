d12e8c661c3a9f2b4a3d8d3e90b9806ab11e8745
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2025-01-01 21:15:32 -0600

    Delete templates/simplemicroservice directory

--- templates/simplemicroservice/itemsMicroservice/src/main/java/com/example/itemsMicroservice/services/ItemService.java
+++
@@ -1,9 +0,0 @@
-package com.example.itemsMicroservice.services;
-
-import java.util.List;
-import main.java.com.example.itemsMicroservice.entities.Item;
-
-public interface ItemService {
-    public List<Item> findAll();
-    public Item findById(Long id, Integer quantity);
-}
