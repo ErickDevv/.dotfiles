67145d92c81399bef8b89fb288ec3c1c384d0dba
ErickDevv <111036821+ErickDevv@users.noreply.github.com>
2026-05-16 10:05:45 -0600

    Add Rofi configuration and theme, update sxhkd shortcuts

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -sf ~/Documentos/.dotfiles/picom ~/.config/
 ln -sf ~/Documentos/.dotfiles/kitty ~/.config/
 ln -sf ~/Documentos/.dotfiles/ranger ~/.config/
 
+ln -sf ~/Documentos/.dotfiles/rofi ~/.config/
+
 ln -sf ~/Documentos/.dotfiles/starship/starship.toml ~/.config/starship.toml
 
 chmod +x ~/.config/bspwm/bspwmrc
